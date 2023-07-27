fb846151e2cc40a593d7ae184fa51de87a8c632f
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-07-27 11:04:23 +0200

    finished code

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,53 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  Color acceptColor = Colors.purple;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              DragTarget<Color>(
+                  onAccept: (color) => setState(() => acceptColor = color),
+                  builder: (BuildContext context, List<Object?> candidateData, List<dynamic> rejectedData) {
+                    return Container(
+                      color: acceptColor,
+                      height: 100,
+                      width: 100,
+                    );
+                  }),
+              Draggable<Color>(
+                data: Colors.blue,
+                feedback: Container(
+                  color: Colors.red,
+                  height: 100,
+                  width: 100,
+                ),
+                childWhenDragging: Container(
+                  color: Colors.blue,
+                  height: 100,
+                  width: 100,
+                ),
+                child: Container(
+                  color: Colors.green,
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
